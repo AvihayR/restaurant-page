5d45d401d3ebbe44a072e87e5b2c5fcd0fe69a80
AvihayR <avihayrabi@gmail.com>
2023-06-20 22:12:09 +0300

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -4,10 +4,32 @@ import header from './header.js';
 import homePage from './homepage.js';
 import menu from './menu.js';
 import contact from './contact.js';
-import footerElement from './footer';
+import footerElement from './footer.js';
+import remove from './remove.js';
 
 header();
 homePage();
 // menu();
 // contact();
 footerElement();
+
+//Query selectors for Tabs:
+const homeTab = document.getElementById('home-tab');
+const menuTab = document.getElementById('menu-tab');
+const contactTab = document.getElementById('contact-tab');
+
+//Add EventListeners:
+homeTab.addEventListener('click', () => {
+  remove();
+  homePage();
+});
+
+menuTab.addEventListener('click', () => {
+  remove();
+  menu();
+});
+
+contactTab.addEventListener('click', () => {
+  remove();
+  contact();
+});
---
+++ src/remove.js
@@ -0,0 +1,6 @@
+export default function remove() {
+  //Fetch content element:
+  const contentDiv = document.getElementById('content');
+
+  contentDiv.innerHTML = '';
+}
